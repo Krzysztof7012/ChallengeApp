82dc6b1a226edf368cca7f01da77da56b79ee456
Krzysztof Gryczewski <gryczewski.k@gmail.com>
2023-01-23 21:26:53 +0100

    Zadanie domowe Dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+var name = "Ewa";
+var sex = "Female";
+var age = 52;
+
+if (sex == "Female" && age < 30);
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+if (name == "Ewa" && age == 33);
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+if (sex != "Female" && age < 18);
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+if (sex != "Female" && age == 52);
+{
+    Console.WriteLine("To potencjalnie może być Krzysztof");
+}
