f8649336b44d83bb320241ac45c1ca84e2e71044
Krzysztof Gryczewski <gryczewski.k@gmail.com>
2023-01-24 22:39:53 +0100

    zadanie dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,30 +1,66 @@
-﻿
+﻿using System.Diagnostics.Metrics;
 
-var name = "Ewa";
-var sex = "Female";
-var age = 33;
+int number = 53453412;
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToArray();
 
-if (sex == "Female" && age < 30)
+
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+foreach (char letter in letters)
+    
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+    if (letter == '0')
+    {
+        counter0 = counter0++;
+    }
+    else if (letter == '1')
+    {
+        counter1++;
+    }
+    else if (letter == '2')
+    {
+        counter2++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+    }
+    else if (letter == '9')
+    {
+        counter9++;
+    }
 }
-else if (name == "Ewa" && age == 33)
-{
-    Console.WriteLine("Ewa, lat 33");
-}
-if (sex != "Female" && age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
-else
-{
-    Console.WriteLine("Pełnoletni mężczyzna");
-}
-if (sex != "Female" && age == 52)
-{
-    Console.WriteLine("To potencjalnie może być Krzysztof");
-}
-else if (sex == "Female" && age < 52)
-{
-    Console.WriteLine("To z pewnością nie jest Krzysztof");
-}
+foreach(char letter in letters)
+    {
+        Console.WriteLine(letter);
+    }
